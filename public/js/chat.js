ebf3853278bbd50983414920288bf8b1cdb2f71d
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-21 00:05:41 +0200

    add:event message

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,10 +3,22 @@ const socket = io()
        1) the name of the event 
        2) the function to run when event occurs
 */
-socket.on('countUpdated' , (count) =>{
+/* socket.on('countUpdated' , (count) =>{
     console.log('countUpdated' , count)
 })
 document.querySelector('#increament').addEventListener('click', ()=>{
     console.log('Clicked')
     socket.emit('increament')
 })
+ */
+socket.on('message',(message)=>{
+    console.log(message)
+})
+document.querySelector('#message-form').addEventListener(
+    'submit',(e) =>{
+        // (e.preventDefault()) --> to prevent default behavior  
+        e.preventDefault()
+       // const message = document.querySelector('input').value
+       const message = e.target.elements.message.value
+       socket.emit('sendMessage',message)
+    })
--- src/index.js
+++ src/index.js
@@ -13,24 +13,29 @@ const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname,'../public')
 
 app.use (express.static(publicDirectoryPath))
-let count = 0 
 //socket is an object that it contains information about that connection  
 /*
     -- server (emit) -> client (receive) -countUpdated
     -- client (emit) -> server (receive) -increment 
 */ 
+var message  = "Welcome"
 io.on('connection',(socket)=>{
     console.log('New WebSocket connection ')
     // send an event from the server to be received  from the client.
     // the event take just one thing which is the name of the event . 
-    socket.emit('countUpdated',count)
-    socket.on('increament', ()=>{
+  /*   socket.emit('countUpdated',count)
+    socket.on('increment', ()=>{
         count++
         /* (socket.emit) emits an event */ 
         // socket.emit('countUpdated' , count)
         /*(io.emit) emits the event to every single connection */
-        io.emit('countUpdated' , count)
-    })
+     //   io.emit('countUpdated' , count) 
+   // }) 
+   io.emit('message',message)
+   socket.on('sendMessage',(message)=>{
+        io.emit('message',message)
+   })
+   console.log(message)
 })
 
 
