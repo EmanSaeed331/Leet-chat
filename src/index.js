3bd9f4bfa5aa41b717a7b8f4019c8492a23625c1
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-21 14:34:51 +0200

    add:event acknowledge

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const console = require('console')
+const Filter = require('bad-words')
 
-//new application http server 
 const app = express()
+//new application http server 
 const server = http.createServer(app)
 const io = socketio(server)
  
@@ -34,13 +35,19 @@ io.on('connection',(socket)=>{
    // }) 
    io.emit('message',message)
    socket.broadcast.emit('message','A new user has joined !' )
-   socket.on('sendMessage',(message)=>{
+   socket.on('sendMessage',(message , callback)=>{
+       const filter = new Filter()
+       if(filter.isProfane(message)){
+           return callback('Profanity is not allowed!')
+       }
         io.emit('message',message)
+        callback()
    })
-    socket.on('sendLocation' , (coords)=>{
+    socket.on('sendLocation' , (coords , callback)=>{
         io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
     })
-   // disconnect is a built in Event
+   // disconnect is a built in Event 
    socket.on('disconnect' , ()=>{
        io.emit('message',' A user has left !')
    })
