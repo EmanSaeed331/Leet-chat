c3cb84cce861482d36063b2be77ba3fe80c7b296
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-21 13:43:45 +0200

    add:fetch location

--- public/js/chat.js
+++ public/js/chat.js
@@ -22,3 +22,13 @@ document.querySelector('#message-form').addEventListener(
        const message = e.target.elements.message.value
        socket.emit('sendMessage',message)
     })
+
+document.querySelector('#send-location').addEventListener('click', ()=>{
+    if(!navigator.geolocation){
+        return alert('Geolocation is not supported by your browser')
+    }
+    navigator.geolocation.getCurrentPosition((position) =>{
+        console.log(position)
+
+    })
+})
