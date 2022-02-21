908f426b9e860cce81184f283fb410fe7bcef26b
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2022-02-21 14:38:28 +0200

    add:bad-words pkg

--- public/js/chat.js
+++ public/js/chat.js
@@ -20,15 +20,24 @@ document.querySelector('#message-form').addEventListener(
         e.preventDefault()
        // const message = document.querySelector('input').value
        const message = e.target.elements.message.value
-       socket.emit('sendMessage',message)
+       socket.emit('sendMessage',message, (error)=>{
+       if(error){
+           return console.log(error)
+       }
+       console.log('Message delivered')
+       })
     })
-
-document.querySelector('#send-location').addEventListener('click', ()=>{
+document.querySelector('#send-location').addEventListener('click', (e)=>{
     if(!navigator.geolocation){
         return alert('Geolocation is not supported by your browser')
     }
     navigator.geolocation.getCurrentPosition((position) =>{
-        console.log(position)
+        socket.emit('sendLocation',{
+            latitude:position.coords.latitude,
+            longitude:position.coords.longitude
+        } , ()=>{
+            console.log('Location shared ')
 
+        })
     })
 })
